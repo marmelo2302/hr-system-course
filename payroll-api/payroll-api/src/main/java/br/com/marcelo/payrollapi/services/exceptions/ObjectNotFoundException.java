bb36920f3eda57378468c5954ffc8e53951301c5
marmelo2302 <marmelo2302@gmail.com>
2023-06-21 20:17:21 -0300

    created docker-compose

--- payroll-api/payroll-api/src/main/java/br/com/marcelo/payrollapi/services/exceptions/ObjectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.marcelo.payrollapi.services.exceptions;
-
-public class ObjectNotFoundException extends RuntimeException {
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-
-}
